6625af4f6837543b9585d92814d4558eb2ed144a
acciohyun <62661863+acciohyun@users.noreply.github.com>
2023-12-15 11:56:40 +0800

    edit interval func to maintain page pos

--- board_art.js
+++ board_art.js
@@ -1,16 +1,17 @@
-
-
-const myInterval = setInterval(rotateImages, 5000)
+const newInterval = setInterval(rotateImages, 5000)
 function rotateImages(){
+    console.log('changed');
+    var myContainer = document.querySelectorAll("[class$='bd']");
     var myArr = document.querySelectorAll("[class$='col']")
     if(!myArr){
         console.log('try again')
         return
     }else{
-        console.log('rotate')
         var index = Math.floor(Math.random() * myArr.length);
-        myArr[index].scrollIntoView();
+        console.log('rotate'+ index);
+        var offsetTop = myArr[index].offsetTop - myContainer[0].offsetTop;
+        console.log(offsetTop)
+        myContainer[0].scrollTop = offsetTop;
+        //myArr[index].scrollIntoView();
     }
 }
-
-    
